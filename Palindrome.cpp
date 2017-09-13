7a6dd7328d70b46ff9f5c684e90547ae5b097e73
Ethan Reese <ethan.reese01@gmail.com>
2017-09-13 10:04:49 -0700

    Made a few changes

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -12,27 +12,19 @@ int main(){
 	char c[LENGTH] = {'0'};
 	//Get the user input and put it into a character array
 	cin >> c;
-	int counter;
 	//Loop through the array and remove all punctuation items
 	for(int a: c){
-		counter = 0;
-
 		//If c at index a is a punctuation delete the character and remove the rest of the characters forward
 		if(ispunct(c[a])){
 			int b = a;
 			for(b : c){
 				//If its about to run into the array index then it needs to 
-				if(b = sizeof(c) - 1){
-
+				if(!(b = sizeof(c) - 1)){
+					c[b] = c[b+1]
 				}
-				c[b] = c[b+1];
-			}
-		}
-		if(c[a] == '0'){
-			counter++;
-			//If there are 10 zeroes in a row
-			if(counter > 10){
-				break;
+				else{
+					c[b] = '0';
+				}
 			}
 		}
 	}
